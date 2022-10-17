76adf8332be11a45ea13ecff04ae410313d8f279
mahmud-hosen <mahmudhossain582@gmail.com>
2022-10-17 21:22:41 +0600

    Added

--- redis_list.php
+++ redis_list.php
@@ -2,11 +2,12 @@
 
 require './vendor/autoload.php';
 $redis = new Predis\Client();
-// echo $redis->ping();
-//  $redis->LPUSH("tutorials", "JS");
-//  $redis->LPUSH("tutorials", "Mahmud");
-//  $redis->LPUSH("tutorials", "Hasan");
-//  $redis->LPUSH("tutorials", "Kamal");
+ 
+ echo $redis->ping();
+ $redis->LPUSH("tutorials", "JS");
+ $redis->LPUSH("tutorials", "Mahmud");
+ $redis->LPUSH("tutorials", "Hasan");
+ $redis->LPUSH("tutorials", "Kamal");
 
  $list = $redis->lrange("tutorials", 0 ,-1);
  for($i=0; $i<= count($list); $i++)
